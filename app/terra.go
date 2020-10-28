af84a31e39e0b5bc28862811facd94dff0f7cd5a
jess <jesse@soob.co>
2020-10-29 01:11:06 +0900

    release/1.3.1

---
+++ app/terra.go
@@ -0,0 +1,31 @@
+package app
+
+import (
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/tendermint/tendermint/libs/log"
+	dbm "github.com/tendermint/tm-db"
+	terra "github.com/terra-project/core/app"
+	"io/ioutil"
+)
+
+func NewTerraApp(db dbm.DB) *terra.TerraApp {
+	return terra.NewTerraApp(
+		log.NewTMLogger(ioutil.Discard),
+		db,
+		nil,
+		true, // need this so KVStores are set
+		0,
+		// make(map[int64]bool),
+		// &wasmconfig.Config{BaseConfig: wasmconfig.BaseConfig{
+		// 	ContractQueryGasLimit: viper.GetUint64(wasmconfig.FlagContractQueryGasLimit),
+		// 	CacheSize:             viper.GetUint64(wasmconfig.FlagCacheSize),
+		// }},
+		fauxMerkleModeOpt, // error
+		// setPruningOptions(),
+	)
+}
+
+// Pass this in as an option to use a dbStoreAdapter instead of an IAVLStore for simulation speed.
+func fauxMerkleModeOpt(bapp *baseapp.BaseApp) {
+	bapp.SetFauxMerkleMode()
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/terra-project/mantle-compatibility
+
+go 1.14
+
+require (
+	github.com/dgraph-io/badger/v2 v2.0.3
+	github.com/tendermint/tm-db v0.2.0
+	github.com/terra-project/core v0.3.7
+)
