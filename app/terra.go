e7a37014065b3bc4b12ab70cccf01c8d8bdc51d9
jess <jesse@terra.money>
2020-12-06 03:09:30 +0900

    release/1.6.0

---
+++ app/terra.go
@@ -0,0 +1,32 @@
+package app
+
+import (
+	"io/ioutil"
+
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/tendermint/tendermint/libs/log"
+	dbm "github.com/tendermint/tm-db"
+	terra "github.com/terra-project/core/app"
+)
+
+func NewTerraApp(db dbm.DB) *terra.TerraApp {
+	return terra.NewTerraApp(
+		log.NewTMLogger(ioutil.Discard),
+		db,
+		nil,
+		true, // need this so KVStores are set
+		0,
+		// make(map[int64]bool),
+		// &wasmconfig.Config{BaseConfig: wasmconfig.BaseConfig{
+		// 	ContractQueryGasLimit: viper.GetUint64(wasmconfig.FlagContractQueryGasLimit),
+		// 	CacheSize:             viper.GetUint64(wasmconfig.FlagCacheSize),
+		// }},
+		// fauxMerkleModeOpt, // error
+		// setPruningOptions(),
+	)
+}
+
+// Pass this in as an option to use a dbStoreAdapter instead of an IAVLStore for simulation speed.
+func fauxMerkleModeOpt(bapp *baseapp.BaseApp) {
+	bapp.SetFauxMerkleMode()
+}
---
+++ types/sdktypes.go
@@ -0,0 +1,26 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	cmn "github.com/tendermint/tendermint/libs/common"
+	tmtypes "github.com/tendermint/tendermint/types"
+	"github.com/terra-project/core/x/genaccounts"
+)
+
+type (
+	Coin           = sdk.Coin
+	Coins          = sdk.Coins
+	Int            = sdk.Int
+	Dec            = sdk.Dec
+	DecCoin        = sdk.DecCoin
+	AccAddress     = sdk.AccAddress
+	ValAddress     = sdk.ValAddress
+	Msg            = sdk.Msg
+	GenesisDoc     = tmtypes.GenesisDoc
+	GenesisAccount = genaccounts.GenesisAccount
+	HexBytes       = cmn.HexBytes
+)
+
+var (
+	NewInt = sdk.NewInt
+)
